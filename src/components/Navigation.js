6938565fef6cfb7cb71fc2376330b3a970bf4178
Roberto <robertochirila@yahoo.com>
2020-06-25 13:32:51 +0100

    added navigation handler

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,19 +1,47 @@
 import React, { Component } from "react";
 
 export class Navigation extends Component {
+  handleNavigation = (event) => {
+    let correspondingStep;
+    if (event.target.id === "Profile") correspondingStep = 1;
+    else if (event.target.id === "Statistics") correspondingStep = 2;
+    else if (event.target.id === "Network") correspondingStep = 3;
+    else if (event.target.id === "Leaderboards") correspondingStep = 4;
+    console.log(correspondingStep);
+  };
   render() {
     return (
       <nav className="nav">
-        <a href="/html/" className="nav-items">
+        <a
+          href="#"
+          id="Profile"
+          className="nav-items"
+          onClick={this.handleNavigation}
+        >
           Profile
         </a>{" "}
-        <a href="/css/" className="nav-items">
+        <a
+          href="#"
+          id="Statistics"
+          className="nav-items"
+          onClick={this.handleNavigation}
+        >
           Statistics
         </a>{" "}
-        <a href="/js/" className="nav-items">
+        <a
+          href="#"
+          id="Network"
+          className="nav-items"
+          onClick={this.handleNavigation}
+        >
           Network
         </a>{" "}
-        <a href="/python/" className="nav-items">
+        <a
+          href="#"
+          id="Leaderboards"
+          className="nav-items"
+          onClick={this.handleNavigation}
+        >
           Leaderboards
         </a>
       </nav>
