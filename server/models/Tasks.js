bef9a9fca0a519985a51547dc86dab0ea85891a6
Roberto <robertochirila@yahoo.com>
2020-06-28 23:15:22 +0100

    added models and routes files

---
+++ server/models/Tasks.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const tasksSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    category: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Tasks = mongoose.model("Tasks", tasksSchema);
+module.exports = Expenses;
---
+++ server/routes/tasks.js
@@ -0,0 +1,56 @@
+const router = require("express").Router();
+let Tasks = require("./Tasks");
+//const auth = require('../middleware/auth')
+
+router.route("/").get((req, res) => {
+  Tasks.find()
+    .then((tasks) => res.json(tasks))
+    .catch((err) => res.status(400).json("Error: " + err));
+});
+
+router.post("/add", auth, (req, res) => {
+  const name = req.body.name;
+  const category = req.body.category;
+
+  const newTasks = new Tasks({
+    name,
+    category,
+  });
+
+  newTasks
+    .save()
+    .then(() => res.json("Task added succesfully"))
+    .catch((err) => res.status(400).json("Error: ", +err));
+});
+/*router.route('/add', auth).post((req, res) => {
+    const name = req.body.name
+    const category = req.body.category
+    const price = req.body.price
+
+    const newExpense = new Expenses({
+        name,
+        category,
+        price
+    })
+
+    console.log(newExpense)
+
+
+    newExpense.save()
+        .then(() => res.json('Expense added succesfully'))
+        .catch(err => res.status(400).json('Error: ', +err))
+
+})*/
+router.delete("/:id", auth, (req, res) => {
+  Tasks.findByIdAndDelete(req.params.id)
+    .then(() => res.json("Task deleted."))
+    .catch((err) => res.status(400).json("Error: " + err));
+});
+
+/*router.route('/:id', auth).delete((req, res) => {
+    Expenses.findByIdAndDelete(req.params.id)
+        .then(() => res.json('Exercise deleted.'))
+        .catch(err => res.status(400).json('Error: ' + err));
+});*/
+
+module.exports = router;
