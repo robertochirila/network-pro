7b564ce753b11f113b9e8f55353825fec10bdeb8
Roberto <robertochirila@yahoo.com>
2020-06-29 16:04:54 +0100

    connected to mongodb

--- server/routes/tasks.js
+++ server/routes/tasks.js
@@ -1,14 +1,14 @@
 const router = require("express").Router();
-let Tasks = require("./Tasks");
+let Tasks = require("../models/Tasks");
 //const auth = require('../middleware/auth')
 
-router.route("/").get((req, res) => {
+router.route("/get").get((req, res) => {
   Tasks.find()
     .then((tasks) => res.json(tasks))
     .catch((err) => res.status(400).json("Error: " + err));
 });
 
-router.post("/add", auth, (req, res) => {
+/*router.post("/add", auth, (req, res) => {
   const name = req.body.name;
   const category = req.body.category;
 
@@ -21,7 +21,7 @@ router.post("/add", auth, (req, res) => {
     .save()
     .then(() => res.json("Task added succesfully"))
     .catch((err) => res.status(400).json("Error: ", +err));
-});
+});*/
 /*router.route('/add', auth).post((req, res) => {
     const name = req.body.name
     const category = req.body.category
@@ -41,11 +41,11 @@ router.post("/add", auth, (req, res) => {
         .catch(err => res.status(400).json('Error: ', +err))
 
 })*/
-router.delete("/:id", auth, (req, res) => {
+/*router.delete("/:id", auth, (req, res) => {
   Tasks.findByIdAndDelete(req.params.id)
     .then(() => res.json("Task deleted."))
     .catch((err) => res.status(400).json("Error: " + err));
-});
+});*/
 
 /*router.route('/:id', auth).delete((req, res) => {
     Expenses.findByIdAndDelete(req.params.id)
--- server/server.js
+++ server/server.js
@@ -1,14 +1,32 @@
 const express = require("express");
-const path = require("path");
-const dotenv = require("dotenv");
-dotenv.config();
+const cors = require("cors");
+const config = require("./config/default.json");
+const dotenv = require("dotenv").config();
+const mongoose = require("mongoose");
+
 const app = express();
-const port = process.env.PORT || 3001;
-const buildPath = path.join(__dirname, "../", "build");
-app.use(express.static(buildPath));
-app.get("*", (req, res) => {
-  res.sendFile(path.join(buildPath, "index.html"));
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+const uri =
+  "mongodb+srv://robertochirila:robertomircea@cluster0-bnujd.gcp.mongodb.net/network-pro-db?retryWrites=true&w=majority";
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("Connection to MongoDB established");
 });
+
+app.use(express.json());
+
+const tasksRouter = require("./routes/tasks");
+//const usersRouter = require('./routes/users')
+//const authRouter = require('./routes/auth')
+
+app.use("/tasks", tasksRouter);
+//app.use('/users', usersRouter)
+//app.use('/auth', authRouter)
+
 app.listen(port, () => {
-  console.log(`Server is up on ${port}`);
+  console.log(`Server running on port ${port}`);
 });
