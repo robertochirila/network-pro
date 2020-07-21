6ee9f54cbd82086452c3814dc48708ad4d1a47f3
Roberto <robertochirila@yahoo.com>
2020-07-21 14:40:12 +0100

    fixed the axios request

--- src/actions/taskActions.js
+++ src/actions/taskActions.js
@@ -3,26 +3,27 @@ import { DISPLAY_TASKS } from "./types";
 import axios from "axios";
 
 export const createTask = (taskData) => (dispatch) => {
-  console.log(taskData);
-
-  fetch("http://localhost:5000/tasks/add", {
-    method: "post",
-    body: JSON.stringify(taskData),
-  })
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      console.log("Fetch Post");
-    });
-
-  dispatch({
-    type: NEW_TASK,
-    payload: taskData,
-  });
+  console.log(taskData)
+  /*axios.post('http://localhost:5000/tasks/add', taskData)
+  .then(res => console.log(res.data))
+  .catch(err => console.log(err.response, err.status, err.message))*/
+  axios.post('http://localhost:5000/tasks/add', taskData)
+            .then(res => dispatch({
+              type: NEW_TASK,
+              payload: taskData,
+            }))
+            .catch(err => console.log(err))
+  
 };
 
 export const displayTasks = () => (dispatch) => {
+
+  /*axios.get('http://localhost:5000/tasks')
+            .then(res => dispatch({
+              type: DISPLAY_TASKS,
+              payload: res.data,
+            }))
+            .catch(err => console.log(err))*/
   dispatch({
     type: DISPLAY_TASKS,
     payload: "",
